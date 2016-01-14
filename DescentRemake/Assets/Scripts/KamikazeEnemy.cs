f7fbd83f27fbea71a5fd78a905fe5b437dfa8f0c
t3vato00 <t3vato00@students.oamk.fi>
2016-01-14 14:13:46 +0200

    Implemented basic kamikaze enemy AI (Work-in-progress)

---
+++ DescentRemake/Assets/Scripts/KamikazeEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KamikazeEnemy : MonoBehaviour {
+
+    public float rotationSpeed;
+    public float movementSpeed;
+    
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(EnemyController.raycastDistance);
+        if (EnemyController.raycastHitThePlayer)
+            MoveEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 moveDirection = player.position - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), rotationSpeed * Time.fixedDeltaTime);
+        transform.position += transform.forward * movementSpeed * Time.fixedDeltaTime;
+    }
+}
