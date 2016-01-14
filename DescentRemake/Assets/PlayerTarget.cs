ee0a5c751a71b4fb369c1fb926b51fbdee853cd7
Topi Karvonen <t3kato00@students.oamk.fi>
2016-01-14 11:38:52 +0200

    Added PlayerTracking class for allowing AIs to discover players.

---
+++ DescentRemake/Assets/PlayerTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerTarget : MonoBehaviour {
+    private static List<PlayerTarget> playerList = new List<PlayerTarget>();
+
+    public IEnumerable<PlayerTarget> players
+    {
+        get { return playerList; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        playerList.Add(this);
+	}
+}
