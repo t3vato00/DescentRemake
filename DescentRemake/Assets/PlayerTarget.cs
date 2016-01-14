467c99d8f3016e8818bb78d9a891c3cc81b24e9a
Topi Karvonen <t3kato00@students.oamk.fi>
2016-01-14 12:21:51 +0200

    Fixed removing player in PlayerTarget class and canonicalized interface.

--- DescentRemake/Assets/PlayerTarget.cs
+++ DescentRemake/Assets/PlayerTarget.cs
@@ -4,13 +4,19 @@ using System.Collections.Generic;
 public class PlayerTarget : MonoBehaviour {
     private static List<PlayerTarget> playerList = new List<PlayerTarget>();
 
-    static public IEnumerable<PlayerTarget> players
+    static public IEnumerable<PlayerTarget> Players
     {
         get { return playerList; }
     }
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         playerList.Add(this);
 	}
+
+    void OnDestroy()
+    {
+        playerList.Remove(this);
+    }
 }
