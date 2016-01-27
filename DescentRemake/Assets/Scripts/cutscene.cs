179c573fd3bbd5a16605292370f850bf4c5e42e4
Tursake <matiaskyriacou@gmail.com>
2016-01-27 14:13:25 +0200

    no message

---
+++ DescentRemake/Assets/Scripts/cutscene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cutscene : MonoBehaviour {
+    public Camera cockpit;
+    public Camera cockpitHUD;
+    public Camera camera1;
+    public Camera camera2;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(cutsceneCamera());
+	}
+
+    IEnumerator cutsceneCamera()
+    {
+        cockpitHUD.enabled = false;
+        cockpit.enabled = false;
+        camera1.enabled = true;
+        camera2.enabled = false;
+        yield return new WaitForSeconds(6);
+
+        cockpit.enabled = false;
+        camera1.enabled = false;
+        camera2.enabled = true;
+        yield return new WaitForSeconds(2);
+
+        cockpitHUD.enabled = true;
+        cockpit.enabled = true;
+        camera1.enabled = false;
+        camera2.enabled = false;
+    }
+}
