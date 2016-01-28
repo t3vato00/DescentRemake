058330f25ffe79cf1d45cae6a5ed82e81d73ee19
Tursake <matiaskyriacou@gmail.com>
2016-01-28 12:19:41 +0200

    Juttuja

--- DescentRemake/Assets/Scripts/cutscene.cs
+++ DescentRemake/Assets/Scripts/cutscene.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class cutscene : MonoBehaviour {
     GameObject player;
+    GameObject spotlight;
     public Camera cockpit;
     public Camera cockpitHUD;
     public Camera camera1;
@@ -11,13 +12,13 @@ public class cutscene : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
+        spotlight = GameObject.Find("Spotlight");
         StartCoroutine(cutsceneCamera());
 	}
 
     IEnumerator cutsceneCamera()
     {
         player.GetComponent<UIController>().enabled = false;
-        player.GetComponent<PlayerMovement>().enabled = false;
         cockpitHUD.enabled = false;
         cockpit.enabled = false;
         camera1.enabled = true;
@@ -29,12 +30,14 @@ public class cutscene : MonoBehaviour {
         camera2.enabled = true;
         yield return new WaitForSeconds(2);
 
+        spotlight.SetActive(false);
         cockpitHUD.enabled = true;
         cockpit.enabled = true;
         camera1.enabled = false;
         camera2.enabled = false;
         yield return new WaitForSeconds(8);
         player.GetComponent<UIController>().enabled = true;
-        player.GetComponent<PlayerMovement>().enabled = true;
+        player.GetComponent<Animator>().enabled = false;
     }
+
 }
