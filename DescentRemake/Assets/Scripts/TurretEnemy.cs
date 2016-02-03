4692e4f40337a6485216ff5c5f273b65afe364b0
t3vato00 <t3vato00@students.oamk.fi>
2016-02-03 11:28:46 +0200

    Work in progress, turret anticipation started

--- DescentRemake/Assets/Scripts/TurretEnemy.cs
+++ DescentRemake/Assets/Scripts/TurretEnemy.cs
@@ -15,17 +15,24 @@ public class TurretEnemy : MonoBehaviour {
     private GameObject nearestPlayer;
     private float nearestPlayerDistance = 1000;
     private FiringWeapons weapons;
+    private HealthShield healthShield;
 
 
 	// Use this for initialization
 	void Start () {
         players = GameObject.FindGameObjectsWithTag("Player");
         weapons = GetComponent<FiringWeapons>();
-
+        healthShield = GetComponent<HealthShield>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //Destroy turret when health is 0
+        if (healthShield.health == 0) {
+            Destroy(this.gameObject);
+        }
+
         foreach (GameObject player in players) {
             if (Vector3.Distance(this.transform.position, player.transform.position) < nearestPlayerDistance)
             {
@@ -35,7 +42,23 @@ public class TurretEnemy : MonoBehaviour {
         }
         if (Vector3.Distance(nearestPlayer.transform.position, this.transform.position) < shootingDistance)
         {
-            Quaternion lookRotation = Quaternion.LookRotation(nearestPlayer.transform.position - this.transform.position, Vector3.up);
+            float distanceToPlayer = Vector3.Distance(nearestPlayer.transform.position, this.transform.position);
+            Vector3 nearestVelocity = nearestPlayer.GetComponent<Rigidbody>().velocity;
+            System.Random randomizer = new System.Random();
+            int anticipationLimit = randomizer.Next(10, 40);
+                                                  //Bullet speed + player speed
+                                                  //Replace bullet speed with parameter when available
+            float travelTimeX = distanceToPlayer / (Mathf.Abs(anticipationLimit) + nearestVelocity.x);
+            float travelTimeY = distanceToPlayer / (Mathf.Abs(anticipationLimit) + nearestVelocity.y);
+            float travelTimeZ = distanceToPlayer / (Mathf.Abs(anticipationLimit) + nearestVelocity.z);
+            float futurePosX = travelTimeX * nearestVelocity.x;
+            float futurePosY = travelTimeY * nearestVelocity.y;
+            float futurePosZ = travelTimeZ * nearestVelocity.z;
+            Vector3 nearestPosition = nearestPlayer.transform.position;
+            Vector3 newPosition = new Vector3(nearestPosition.x + futurePosX, nearestPosition.y + futurePosY, nearestPosition.z + futurePosZ);
+
+            //Quaternion lookRotation = Quaternion.LookRotation(nearestPlayer.transform.position - this.transform.position, Vector3.up);
+            Quaternion lookRotation = Quaternion.LookRotation(newPosition - this.transform.position, Vector3.up);
             Quaternion relativeLookRotation = lookRotation * Quaternion.Inverse(transform.rotation);
             Vector3 relativeTargetRotationAxis; float relativeTargetRotationAngle;
             relativeLookRotation.ToAngleAxis(out relativeTargetRotationAngle, out relativeTargetRotationAxis);
