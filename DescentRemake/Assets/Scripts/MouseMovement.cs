1c2188f802d49b94a7b28c6f330e9530da745648
t3laja00 <t3laja00@students.oamk.fi>
2016-02-04 12:25:13 +0200

    MouseMovement.cs fixed. Added functions to PlayerMovement.cs

--- DescentRemake/Assets/Scripts/MouseMovement.cs
+++ DescentRemake/Assets/Scripts/MouseMovement.cs
@@ -3,37 +3,16 @@ using System.Collections;
 
 [AddComponentMenu("Camera-Control/Mouse Look")]
 public class MouseMovement : MonoBehaviour {
-	
-	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2}
-	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 5F;
-	public float sensitivityY = 5F;
-	public float minimumX = -360F;
-	public float maximumX = 360F;
-	public float minimumY = -360000F;
-	public float maximumY = 360000F;
-	float rotationY = 360F;
-	void Update ()
+
+	public float horizontalSpeed = 4.0f;
+	public float verticalSpeed = 4.0f;
+	float h;
+	float v;
+
+	void FixedUpdate ()
 	{
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-		}
-		else
-		{
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-		}
+		h = horizontalSpeed * Input.GetAxis ("Mouse X");
+		v = verticalSpeed * Input.GetAxis ("Mouse Y");
+		transform.Rotate (-v, h, 0);
 	}
 }
