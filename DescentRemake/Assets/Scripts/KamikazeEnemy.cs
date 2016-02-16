4a6bcf4e0904154779e9a300a4553f19593eaa30
t3vato00 <t3vato00@students.oamk.fi>
2016-02-16 09:25:45 +0200

    Non patrolling kamikaze enemy.

--- DescentRemake/Assets/Scripts/KamikazeEnemy.cs
+++ DescentRemake/Assets/Scripts/KamikazeEnemy.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class KamikazeEnemy : MonoBehaviour {
 
 	public float spottingDistance = 50;
+	public int damageAmount = 50;
+	public float speed = 5;
 	private GameObject nearestPlayer;
 	private GameObject[] players;
 
 	private float nearestPlayerDistance = 99999;
+
+	private GameObject instantedObj;
+	[SerializeField]
+	private GameObject missileExplosion;
 	// Use this for initialization
 	void Start () {
 		players = GameObject.FindGameObjectsWithTag ("Player");
+
 	}
 	
 	// Update is called once per frame
@@ -26,7 +33,22 @@ public class KamikazeEnemy : MonoBehaviour {
 	void FixedUpdate() {
 		if (nearestPlayerDistance < spottingDistance) {
 			transform.LookAt (nearestPlayer.transform);
-			GetComponent<Rigidbody> ().AddForce (transform.forward * 10);
+			GetComponent<Rigidbody> ().AddForce (transform.forward * speed);
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.GetComponent<HealthShield> () != null) {
+			other.gameObject.GetComponent<HealthShield> ().takeDmg (damageAmount);
+			missileExplosion.transform.localScale = new Vector3 (2f, 2f, 2f);
+			instantedObj = (GameObject)Instantiate (missileExplosion, this.transform.position, this.transform.rotation);
+			Destroy (instantedObj, 1.0f);
+			Destroy (this.gameObject);
+		} else {
+			missileExplosion.transform.localScale = new Vector3 (2f, 2f, 2f);
+			instantedObj = (GameObject)Instantiate (missileExplosion, this.transform.position, this.transform.rotation);
+			Destroy (instantedObj, 1.0f);
+			Destroy (this.gameObject);
 		}
 	}
 }
